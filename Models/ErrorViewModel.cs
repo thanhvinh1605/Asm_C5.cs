e9749ed252247913dcdb50ce28aa6600c3128cb4
thanhvinh1605 <vinhttph19362@fpt.edu.vn>
2024-01-18 18:48:47 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asm_C5.cs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
